ad1bb2a7d532cc9e9fc84e6c6931fa3980136f79
FGSSmap <fgss.map@gmail.com>
2025-08-02 16:41:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-document.getElementById("campus-button").addEventListener("click",() => {
+//switchDisplayの指定
+document.getElementById("campus-button").addEventListener("click", () => {
   switchDisplay("campus");
 });
 
-document.getElementById("jp-button").addEventListener("click",()=>{
+document.getElementById("jp-button").addEventListener("click", () => {
   switchDisplay("japan");
 });
 
-function switchDisplay(target){
+function switchDisplay(target) {
   const campusMap = document.getElementById("campus-map");
   const japanMap = document.getElementById("japan-map");
 
-  campusMap.style.display = target === "campus" ? "black" : "none";
-  japanMap.style.display = target === "japan" ? "black" : "none";
-};
+  // 表示切り替え
+  campusMap.style.display = target === "campus" ? "block" : "none";
+  japanMap.style.display = target === "japan" ? "block" : "none";
 
+  //中身の切り替え
+  if (target === "campus") {
+    campusMap.innerHTML = `<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1nTgYFWkXf1UQHwGZCwdXuRv-aopgUkY&ehbc=2E312F" width="640" height="480"></iframe>`;
+  }
 
-if (target === "campus"){
-  campusMap.innerHTML = `<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1nTgYFWkXf1UQHwGZCwdXuRv-aopgUkY&ehbc=2E312F" width="640" height="480"></iframe>`;
+  if (target === "japan") {
+    fetch('japan-map.svg')
+      .then(response => response.text())
+      .then(svgData => {
+        japanMap.innerHTML = svgData;
+      })
+      .catch(err => {
+        japanMap.innerHTML = `<p>日本地図の読み込みに失敗しました</p>`;
+        console.error(err);
+      });
+  }
 }
-
-if (target === "japan"){
-  fetch('japan-,map.svg')
-  .then(response => response.text())
-  .then(svgData => {
-    japanMap.innerHTML = svgData;
-  })
-  .catch(err => {japanMap.innerHTML = `<p>日本地図の読み込みに失敗しました</p>`;
-                console.error(err);
-                });
-}}
