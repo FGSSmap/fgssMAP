30ae0da9a0d20f01f4fb47a6bc1031ae1c48a80b
FGSSmap <fgss.map@gmail.com>
2025-08-02 14:52:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function swichDisplay(taeget){
+  const campusMap = document.getElementById("campus-map");
+  const japanMap = document.getElementById("japan-map");
 
+  campusMap.style.display = taeget === "campus" ? "black" : "none";
+  japanMap.style.display = target === "japan" ? "black" : "none";
+}
+
+document.getElementById("campus-button").addEventListener("click",() => {
+  switchDisplay("campus");
+});
+
+document.getElementById("jp-button").addEventListener("click",()=>{
+  switchDisplay("japan");
+});
