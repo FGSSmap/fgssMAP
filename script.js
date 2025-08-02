d9a39ae593b541d0c5b9f121f7d0718ece7ee5a5
FGSSmap <fgss.map@gmail.com>
2025-08-02 16:17:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ document.getElementById("jp-button").addEventListener("click",()=>{
   switchDisplay("japan");
 });
 
-function swichDisplay(taeget){
+function switchDisplay(target){
   const campusMap = document.getElementById("campus-map");
   const japanMap = document.getElementById("japan-map");
 
-  campusMap.style.display = taeget === "campus" ? "black" : "none";
+  campusMap.style.display = target === "campus" ? "black" : "none";
   japanMap.style.display = target === "japan" ? "black" : "none";
 };
 
 
+if (target === "campus"){
+  campusMap.innerHTML = `<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1nTgYFWkXf1UQHwGZCwdXuRv-aopgUkY&ehbc=2E312F" width="640" height="480"></iframe>`;
+}
+
+if (target === "japan"){
+  fetch('japan-,map.svg')
+  .then(response => response.text())
+  .then(svgData => {
+    japanMap.innerHTML = svgData;
+  })
+  .catch(err => {japanMap.innerHTML = `<p>日本地図の読み込みに失敗しました</p>`;
+                console.error(err);
+                });
+}}
