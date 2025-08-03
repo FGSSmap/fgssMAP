cfd7987d5cb03e51ba4eb866a907aac6dc952b44
FGSSmap <fgss.map@gmail.com>
2025-08-03 13:25:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -101,6 +101,6 @@ fetch("https://fgssmap.github.io/fgssMAP/map-links.json")
 function showPrefectureMap(code) {
   const url = mapLinks[code];
   if (!url) {
-    document.getElementById("maprange").innerHTML = "<p>この都道府県の地図はまだ準備中です。</p>";
+    document.getElementById("campus-map").innerHTML = "<p>この都道府県の地図はまだ準備中です。</p>";
     return;
   }
