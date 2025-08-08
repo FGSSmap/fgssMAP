5ace4d45567808b42562f1a79c40cf16c176ec2c
FGSSmap <fgss.map@gmail.com>
2025-08-08 18:32:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -196,3 +196,41 @@ function loadAndDisplayPlacemarks(kmlPath) {
       console.error("KML読み込みエラー:", err);
     });
 }
+
+function loadAndDisplayPrefPlacemarks(code){
+  const placemarkContainer = document.getElementById("placemarks-list");
+  placemarkContainer.style.display = "flex";
+  placemarkContainer.innerHTML = "";
+
+  const kmlPath = `placemark/${code}.kml`;
+  fetch(kmlPath)
+  .then(response => {
+    if (!response.ok)throw new Error("KMLの読み込みに失敗しました");
+    return response.text();
+  })
+  .then(str => new window.DOMParser().parseFromString(str, "text/kml"))
+  .then(data => {
+    const documentTag = data.getElementsByTagName("Document")[0];
+    if(!documentTag){
+      console.warn("Documentタグが見つかりません");
+      return;
+    }
+    const placemarks = documentTag.getElementsByTagName("Placemark");
+
+    for (const placemark of placemarks){
+        const name = placemark.getElementsByTagName("name")[0]?.textContent || "名称不明";
+        const descNode = placemark.getElementsByTagName("description")[0];
+        const desc = descNode ? descNode.innerHTML : "";
+        const div = document.createElement("div");
+        div.className = "placemark-box";
+        div.innerHTML = `
+          <h3>${name}</h3>
+          <p><small>${desc}</small></p>
+        `;
+        placemarkContainer.appendChild(div);
+    }
+  })
+  .catch(err => {
+    console.error("KML読み込みエラー:", err);
+  });
+}
