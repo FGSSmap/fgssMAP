7c787e0b024a55e94ddc15c05cd1a91a0e9ca367
FGSSmap <fgss.map@gmail.com>
2025-08-09 14:08:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -200,27 +200,32 @@ function loadAndDisplayPlacemarks(kmlPath) {
 function loadAndDisplayPrefPlacemarks(code){
   const placemarkContainer = document.getElementById("placemarks-list");
   placemarkContainer.style.display = "flex";
-  placemarkContainer.innerHTML = "";
 
-  const kmlPath = `placemark/${code}.kml`;
-  fetch(kmlPath)
+  const kmlPathP = `placemark/${code}.kml`;
+  fetch(kmlPathP)
   .then(response => {
     if (!response.ok)throw new Error("KMLの読み込みに失敗しました");
     return response.text();
   })
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
+    placemarkContainer.innerHTML = "";
+    
     const documentTag = data.getElementsByTagName("Document")[0];
     if(!documentTag){
       console.warn("Documentタグが見つかりません");
       return;
     }
+    
     const placemarks = documentTag.getElementsByTagName("Placemark");
 
     for (const placemark of placemarks){
         const name = placemark.getElementsByTagName("name")[0]?.textContent || "名称不明";
         const descNode = placemark.getElementsByTagName("description")[0];
         const desc = descNode ? descNode.innerHTML : "";
+        const coords = placemark.getElementsByTagName("coordinates")[0]?.textContent.trim() || "";
+        const [lng, lat] = coords.split(",");
+      
         const div = document.createElement("div");
         div.className = "placemark-box";
         div.innerHTML = `
